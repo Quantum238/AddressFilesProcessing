de8f04bf123ef07fcd7ca8b950b8e7f4603462a3
Danny Pinghero <pinghedm@gmail.com>
2014-01-31 18:48:49 -0500

    Added comments and docstrings to latex making file

--- latex_making_V1.py
+++ latex_making_V1.py
@@ -16,11 +16,15 @@ junk_dir = os.path.join(main_dir,'junk')
 
 
 def write_preamble(f,outputname):
+    """Sets up the preamble of the latex document."""
+
+    #sets pdf file name
     f.write(r"%& -job-name="+outputname)
     f.write('\n')
     f.write('\n')
     f.write(r"\documentclass[landscape]{article}")
     f.write('\n')
+    #suppresses most of the LaTeX output
     f.write(r"\batchmode")
     f.write('\n')
     f.write(r"\usepackage{graphicx}")
@@ -29,8 +33,10 @@ def write_preamble(f,outputname):
     f.write('\n')
     f.write(r"\usetikzlibrary{calc}")
     f.write('\n')
+    #tells LaTeX where the pictures are
     f.write(r"\graphicspath{{data/indicias/}}")
     f.write('\n')
+    #turns off page numbers
     f.write(r"\pagenumbering{gobble}")
     f.write('\n')
     f.write(r"\begin{document}")
@@ -38,15 +44,16 @@ def write_preamble(f,outputname):
     f.write('\n')
 
 def insert_one_picture(f,image,pos):
-    #'Private Function'
-    #Inserts One Picture at a corner of your choice
-    #f is the file handle to which you are writing
-
-    #Requires that both logos and indicias all be in the
-    #indicias directory
-    #I should probably rename that to images or some shit
+    """'Private Function'
+    Inserts One Picture at a corner of your choice
+    f is the file handle to which you are writing
+    """
     
     fullpath = os.path.join(indicia_dir,image)
+
+    #this is from the PIL library.  I need to grab the sizes of
+    #the images because LaTeX can be...douchy...about displaying images
+    
     img = Image.open(fullpath)
     width,height = img.size
 
@@ -65,13 +72,14 @@ def insert_one_picture(f,image,pos):
 
     
 def add_pics(f,imagename,logo=None):
-
-    #'Public' function
-    #Adds header images to each page
-    #Give name of indicia file and extension
-    #and logo, if required
+    """
+    'Public' function
+    Adds header images to each page
+    Give name of indicia file and extension, but not dir path.
+    Also adds logo, if required
 
     #f is the file to which you are writing
+    """
 
     if not logo:
         insert_one_picture(f,imagename,'north east')
@@ -82,15 +90,23 @@ def add_pics(f,imagename,logo=None):
         f.write('\n\n')
 
 def address_preamble(f):
+    """Writes the part that defines the beginning of each address page."""
 
+    #vertical centering
     f.write(r"\vspace*{\fill}")
     f.write('\n')
+    #horizontal centering
     f.write(r"\begin{centering}")
     f.write('\n')
+    #font size
     f.write(r"\begin{Large}")
     f.write('\n')
     
 def address_bottom_shit(f,ii,num_copies,job_id):
+    """Writes that part that closes up each address page"""
+
+    #This closes the text controls.  Should probably be
+    #variables and linked to the preamble function
 
     f.write(r"\end{Large}")
     f.write('\n')
@@ -100,6 +116,7 @@ def address_bottom_shit(f,ii,num_copies,job_id):
     f.write('\n')
 
 
+    #Add the page number count and the copy count
     f.write(r"\begin{tikzpicture}[remember picture,overlay]")
     f.write('\n')
     f.write(r"\node[anchor=south west]")
@@ -111,7 +128,7 @@ def address_bottom_shit(f,ii,num_copies,job_id):
     f.write(r"\end{tikzpicture}")
     f.write('\n')
       
-    
+    #add the job id 
     f.write(r"\begin{tikzpicture}[remember picture,overlay]")
     f.write('\n')
     f.write(r"\node[anchor=south east]")
@@ -128,12 +145,18 @@ def address_bottom_shit(f,ii,num_copies,job_id):
 
     
 def make_an_address_page(f,ii,line,job_id):
+    """Call all the functions in order required to create one address page."""
+
 
     address_preamble(f)
-
     num_copies = line.pop()
-    ##This assumes NumCopies is the last thing in the csv
-    ##It also assumes its the string of a plain integer
+    ##This assumes NumCopies is the last thing in the csv,
+    #which is sort of guaranteed.
+    #The Reformat addresses function will put NumCopies
+    #at the end if it exists, but it doesn't have to exist
+
+    ##It also assumes its the string of a plain integer, which is
+    #technically not guaranteed
     
     for item in line:
         if item!='':
@@ -145,6 +168,10 @@ def make_an_address_page(f,ii,line,job_id):
     
 
 def start_page(f):
+
+    #This is the page at the beginning of the carrier section
+    #currently it takes no inputs because I don't sort based
+    #on carriers yet
 
     f.write(r"\begin{large}")
     f.write('\n')
@@ -158,6 +185,9 @@ def start_page(f):
 
 def bulk_page(f,num):
 
+    #This is the page at the beginning
+    #of the bulk page section
+    
     f.write(r"\begin{large}")
     f.write('\n')
     f.write(r"Bulk Copies Start Sheet\\")
@@ -171,6 +201,9 @@ def bulk_page(f,num):
     f.write('\n')
 
 def non_bulk_page(f,num):
+
+    #This is the page at the beginning
+    #of the single page section
 
     f.write(r"\begin{large}")
     f.write('\n')
@@ -186,6 +219,8 @@ def non_bulk_page(f,num):
 
 def end_page(f):
 
+    #This page goes at the end of each carrier's section
+    
     f.write(r"\begin{large}")
     f.write('\n')
     f.write(r"Carrier End Sheet")
@@ -197,11 +232,17 @@ def end_page(f):
     f.write('\n')    
 
 def end_doc(f):
+
+    #the last line of the tex doc
     f.write(r"\end{document}")
 
     
     
 def create_document(filename):
+
+    #calls pdf latex from the command line with
+    #appropriate output shuffling arguments
+    
     code = call(['pdflatex',
              '-output-directory='+pdf_dir,
              '-aux-directory='+junk_dir,

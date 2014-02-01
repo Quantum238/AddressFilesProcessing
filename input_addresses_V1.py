d10923bae1b2b0f972135edc195e5dc67a127e10
Danny Pinghero <pinghedm@gmail.com>
2014-02-01 11:43:33 -0500

    Renamed V2 files to V1 as they passed all tests

--- input_addresses_V1.py
+++ input_addresses_V1.py
@@ -1,65 +1,85 @@
 import csv
 import os
-import random
-import math
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = os.path.join(main_dir,'data')
 address_dir = os.path.join(data_dir,'address_files')
 
-
-
 def get_reader(filename):
-    """Returns a csv reader and a file_handle to input file, assuming
-    it exists in the address_dir.  Catastrophic failure otherwise
+    """'Public Function'
+    Return a csv reader and a file handle for address files.
 
-    'Private' function
+    Assumes file exists in the address_dir directory.
+    Otherwise it will throw an unhandled exception.
     """
-    
+
     f = open(os.path.join(address_dir,filename),'r+')
     add_reader = csv.reader(f)
     return add_reader,f
 
+def generate_job_id(last_num=0):
+    """'Private Function'
+    Create a new job id number.
 
-def generate_job_id(cache = []):
-    """Creates a new job id number from a random number.
-
-    You'll notice the cache in the input.  This doesn't work, because it's
-    not preserved across code invocations.  If you do several files in one
-    instantiation of the interactive editor, you will be guaranteed unique
-    ID's.  If you don't, you'll probably be safe because it seeds from the
-    system time, but this is not acceptable for production code.  Probably?
-    I have to seriously consider this.  Maybe pickle the cache?
+    May need to make this thread safe later, but for now this simply reads
+    in a value from main code and increments it.
     """
     
-    new_id = math.ceil(random.random() * 10**5)
-    if new_id in cache:
-        new_id = generate_job_id()
-    cache.append(new_id)
-    return new_id
+    return last_num+1
 
-def define_mapping(x,header):
-    """Changes the name of a column header to a user input one.
-    Will only accept a canonical header.
+def load_job_id():
+    """'Private Function'
+    Read job id in from [implementation not yet decided]
+
     """
-    
-    print(x,' is not a standard header name.\n')
-    new = 'elephant man'
-    while new not in header:
-        new = input('What should it map to? ')
-    return new
-    
+    pass
 
-def reformat_address_file(reader,file):
-    """Reformat is a bit of a misnomer at this point.
-    Standardizes and correctly orders the header row,
-    then reorders columns to match.  Along the way,
-    counts various things and creates two additional
-    csv files to store country ordering and bulk/single data
+def save_job_id():
+    """'Private Function'
+    Updates persistent copy of last job id number.  Counterpart to load_job_id
+    [implementation not yet decided upon]
+
+    """
+    pass
+
+def get_next_job_id():
+    """'Public Function'
+    Returns the next job id number.  Guaranteed to be unique.
+
+    """
+    load_job_id()
+    job_number = generate_job_id()
+    save_job_id()
+
+    return job_number
+
+def define_mapping(x,header,cache = {}):
+    """'Private Function'
+    Changes the name of a column header to either a value from the cache or,
+    if none defined, asks the user for one and updates the cache.
+
+    May need to be less free with the auto cacheing.  It is possible, I suppose
+    for various customers to incorrectly name columns in contradictory ways.
+
     """
 
-    #canonical titles and order or keyrow
-    header = ['Other 1',
+    if x in cache.keys():
+        return cache[x]
+    else:
+        print(x,' is not a standard header name.\n')
+        new = 'elephant man'
+        while new not in header:
+            new = input('What should it map to? ')
+        cache[x] = new
+        return new
+
+def validate_header(keyline):
+    """'Private Function'
+    Checks header row to see if it conforms to specs.  Reorders
+    as necessary.  Returns shuffle order
+
+    """
+    header_row = ['Other 1',
               'Other 2',
               'Name',
               'Title',
@@ -71,87 +91,111 @@ def reformat_address_file(reader,file):
               'State',
               'Zip',
               'Country',
-              'Number of Copies']
-                      
-
-    #Initialize some things
-    title_row = next(reader)
+              'Number of Copies'
+              ]
     sort_helper = []
+
+    for x in keyline:
+        if x in header_row:
+            sort_helper.append(header_row.index(x))
+        elif x!='' and x not in header_row:
+            new_header = define_mapping(x,header_row)
+            sort_helper.append(header.index(new_header))
+
+    return sort_helper,header_row
+
+def validate_country(country,cache = {}):
+    """'Private Function'
+    Ensures country is a proper country (none of that South Volta nonesense)
+
+    """
+
+    #maybe read this in from a file or something?
+    acceptable_countries = []
+    return country #for now
+
+##    if country in accepable_countries:
+##        return country
+##    elif country in cache.keys():
+##        return cache[country]
+##    else:
+##        print(country,' is not a known country.  If you would like to map it \
+##to something else, type in the name of a known country.  If you would like to \
+##add it as is, enter 0\n')
+##        new_name = 'elephant man'
+##        while new_name not in acceptable_countries and new_name != 0:
+##            new_name = input('New Name: ')
+##        if new_name == '0':
+##            #this function doesn't exist yet
+##            update_countries_list(new_name)
+##        else:
+##            cache[country] = new_name
+##
+##        return new_name
+
+def update_countries_list(country):
+    """'Private Function
+    Updates the list of acceptable countries.
+    Implementation not yet decided upon.
+
+    """
+
+    pass
+    
+
+def process_address_file(reader,file):
+    """'Public Function'
+    Takes a raw address file from customer, produces a new file that conforms
+    to assumptions, and collects/outputs some data about the file
+
+    """
+
     mult_copies_list = []
     countries = []
-    incredulity_counter = 0
+    row_counter = 0
     total_num_mags = 0
+    countries = {}
+    new_file = os.path.join(address_dir,'temp.csv')
 
+    keyline = next(reader)
+    sort_helper,header_row = validate_header(keyline)
 
-    #ensure title row is in correct order and contains correct headings
-    for x in title_row:
-        if x!='' and x in header:
-            sort_helper.append(header.index(x))
-        elif x!='' and x not in header:
-            new_header = define_mapping(x,header)
-            sort_helper.append(header.index(new_header))
-            
+    with open(new_file,'w',newline = '') as f:
+        writer = csv.writer(f)
+        writer.writerow(header_row)
 
-    #temp will be the new main csv
-    temp = os.path.join(address_dir,'temp.csv')
-    
-    #these two hold the orders of bulk and single
-    bulk_country = os.path.splitext(file.name)[0]+'_btemp.csv'
-    single_country = os.path.splitext(file.name)[0]+'_stemp.csv'
-
-    #open all these lovely files
-    with open(temp,'w',newline = '') as new_file, open(bulk_country,'w',newline = '') as bulk_country,open(single_country,'w',newline='') as single_country:
-
-        #get writers for all necessary files
-        writer = csv.writer(new_file)
-        bc_writer = csv.writer(bulk_country)
-        sc_writer = csv.writer(single_country)
-
-        
-        writer.writerow(header)
-
-        #shuffle every row according to sort_helper and write it into temp
-        #also count how many magazines there are
         for line in reader:
             new_line = [x for (y,x) in sorted(zip(sort_helper,line))]
-            writer.writerow(new_line)
+
             num_copies = int(new_line[-1])
-            total_num_mags+=num_copies
-
-
-            #countries go into separate files depending on bulk or singleton
-            #This ends up being not necessary I think, after I refactor it
-            #But for now, it is easiest.
+            total_num_mags += num_copies
+            
             country = new_line[-2]
-            if num_copies>1:
-                bc_writer.writerow([country])
-            elif num_copies==1:
-                sc_writer.writerow([country])
-                
+            country = validate_country(country)
+            try:
+                countries[country] += 1
+            except KeyError:
+                countries[country] = 1
+            
 
-            
-            #if there is more than 1 copy, we need to know about that,
-            #and where it happens
             if num_copies>1:
-                mult_copies_list.append(incredulity_counter)
+                mult_copies_list.append(row_counter)
+            
+            writer.writerow(new_line)
+            row_counter += 1
 
-            incredulity_counter+=1
-            
-            
-        
-            
-    #close the initial address file
+
     file.close()
-    #delete it
     os.remove(file.name)
-    #rename the newly formatted temp file to what the old address file was
-    os.rename('C:\\Users\\DannyBrosef\\Desktop\\MyCodes\\CrystalReportsClone\\data\\address_files\\temp.csv',
-              file.name)
+    os.rename(new_file,file.name)
 
-    return file.name,mult_copies_list,incredulity_counter,total_num_mags
+    return file.name,mult_copies_list,row_counter,total_num_mags,countries
+
+        
+            
+
     
-            
-        
-        
-            
+
+    
+
     
